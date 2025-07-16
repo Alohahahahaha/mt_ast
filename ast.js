ec256bcc6b380814cf6a3e7f954c0964a57dcd5c
Alohahahahaha <2357349368@qq.com>
2025-07-16 13:17:07 +0800

    i++

--- ast.js
+++ ast.js
@@ -8,6 +8,7 @@
 
 const vm = require('vm');
 const files = require('fs');
+const {minify} = require("terser");
 const types = require("@babel/types");
 const parser = require("@babel/parser");
 const traverse = require("@babel/traverse").default;
@@ -79,10 +80,11 @@ class MeiTua {
                     let lpg = generator(kyb.node).code;
                     this.transCode = 'var ' + lpg + '\n' + huv + '\n';
                     eval(this.transCode);
-                    kyb.remove();
+                    // kyb.remove();
                 }
                 this.transFunc = id.name;
-                path.remove()
+                // path.remove()
+                path.stop()
             }
         })
     }
@@ -122,24 +124,106 @@ class MeiTua {
     }
 
     mtg(mainPath, id) {
+        let ptn = function (path, key, test, consequent) {
+            let f, g, h, i, j, k, l, m, n, o, p, q, r;
+            f = path.parentPath;
+            if (f.node.body.length === 1) return;
+            if (key !== 1) {
+                p = test.right;
+                q = types.numericLiteral(test.right.value + 1);
+                if (!types.isBinaryExpression(f.node.body[0].test)) return;
+            } else {
+                p = types.numericLiteral(test.right.value + 1);
+                q = test.right;
+            }
+            if (types.isIfStatement(f.node.body[1]) && f.node.body.length !== 2) {
+                m = f.node.body[0];
+                r = f.node.body[1];
+                g = f.node.body.slice(2);
+            } else {
+                g = f.node.body.slice(1);
+            }
+            if (key === 1 && f.node.body.length === 2 && types.isIfStatement(f.node.body[1])) {
+                let a, b, c, d, e, aa;
+                aa = f.get('body.1');
+                aa.remove();
+                a = g[0].test.right.value;
+                b = types.binaryExpression('===', types.identifier(id), types.numericLiteral(a));
+                c = types.binaryExpression('===', types.identifier(id), types.numericLiteral(a + 1));
+                d = types.ifStatement(c, types.blockStatement(g[0].alternate.body), null);
+                d.isHandle = true;
+                e = types.ifStatement(b, types.blockStatement(g[0].consequent.body), types.blockStatement([d]));
+                e.isHandle = true;
+                path.node.alternate = types.blockStatement([e]);
+                f.replaceWith(types.blockStatement([path]));
+                return;
+            }
+            if (key === 1 && f.node.body.length !== 2 && types.isIfStatement(f.node.body[1])) {
+                let a, b, c, d, e;
+                a = r.test.right.value;
+                b = types.binaryExpression('===', types.identifier(id), types.numericLiteral(a + 1));
+                c = types.binaryExpression('===', types.identifier(id), types.numericLiteral(a));
+                d = types.ifStatement(b, types.blockStatement(g), null);
+                d.isHandle = true;
+                e = types.ifStatement(c, r.consequent, types.blockStatement([d]));
+                e.isHandle = true;
+                m.alternate = types.blockStatement([e]);
+                f.replaceWith(types.blockStatement([m]));
+                return;
+            }
+            h = types.identifier(id);
+            i = types.binaryExpression('===', h, p);
+            j = types.binaryExpression('===', h, q);
+            k = types.ifStatement(j, types.blockStatement(g), null);
+            k.isHandle = true;
+            l = types.ifStatement(i, consequent, types.blockStatement([k]));
+            l.isHandle = true;
+            if (types.isIfStatement(f.node.body[1]) && f.node.body.length !== 2) {
+                n = types.binaryExpression(m.test.operator, m.test.left, m.test.right);
+                o = types.ifStatement(n, types.blockStatement(m.consequent.body), types.blockStatement([l]));
+                o.isHandle = true;
+                f.replaceWith(types.blockStatement([o]));
+                return;
+            }
+            f.replaceWith(types.blockStatement([l]));
+        };
         traverse(mainPath.node, {
             IfStatement: (path) => {
                 let {test, consequent, alternate} = path.node;
                 if (path.node.isHandle) return;
                 if (!types.isBinaryExpression(test)) {
                     if (!types.isUnaryExpression(test)) return;
-                    let a, b, c;
-                    a = test.argument;
-                    b = types.binaryExpression('<=', types.identifier(id), types.numericLiteral(a.right.value));
-                    c = types.ifStatement(b, types.blockStatement(consequent.body), null);
-                    path.replaceWith(c);
+                    let a, b, c, d, e, f;
+                    if (test.argument.operator.includes('>')) {
+                        a = test.argument;
+                        b = types.binaryExpression('===', types.identifier(id), types.numericLiteral(a.right.value));
+                        c = types.ifStatement(b, types.blockStatement(consequent.body), null);
+                        c.isHandle = true;
+                        path.replaceWith(c);
+                        ptn(path, 1, a, consequent);
+                        return;
+                    }
+                    d = test.argument;
+                    e = types.binaryExpression('===', types.identifier(id), types.numericLiteral(d.right.value + 1));
+                    f = types.ifStatement(e, types.blockStatement(consequent.body), null);
+                    f.isHandle = true;
+                    path.replaceWith(f);
+                    ptn(path, 1, d, consequent);
                     return;
                 }
                 if (test.left.name !== id) return;
                 if (types.isExpressionStatement(consequent.body[0]) && types.isLogicalExpression(consequent.body[0].expression)) {
                     let {left, operator, right} = consequent.body[0].expression;
                     if (!types.isBinaryExpression(left)) return;
-                    if (operator !== '||') return;
+                    if (!['||', '&&'].includes(operator)) return;
+                    if (test.operator === '>' && operator === '&&') {
+                        let a, b;
+                        a = types.binaryExpression('===', left.left, types.numericLiteral(left.right.value + 1));
+                        b = types.ifStatement(a, types.blockStatement([types.expressionStatement(right)]), null);
+                        b.isHandle = true;
+                        consequent.body[0] = b;
+                        return;
+                    }
                     test.operator = '===';
                     consequent.body[0] = types.expressionStatement(right);
                     return;
@@ -155,19 +239,13 @@ class MeiTua {
                     return;
                 }
                 if (test.operator === '<=' && !types.isIfStatement(consequent.body[0])) {
-                    let a, b, c, d, e, f, g, h, i, j, k, l;
+                    let a, b, c, d, e;
                     if (alternate === null) {
-                        f = path.parentPath;
-                        if (f.node.body.length === 1) return;
-                        g = f.node.body.slice(1);
-                        h = types.identifier(id);
-                        i = types.binaryExpression('===', h, test.right);
-                        j = types.binaryExpression('===', h, types.numericLiteral(test.right.value + 1));
-                        k = types.ifStatement(j, types.blockStatement(g), null);
-                        k.isHandle = true;
-                        l = types.ifStatement(i, consequent, types.blockStatement([k]));
-                        l.isHandle = true;
-                        f.replaceWith(l);
+                        ptn(path, 0, test, consequent);
+                        return;
+                    }
+                    if (types.isIfStatement(alternate.body[0])) {
+                        path.node.test.operator = '===';
                         return;
                     }
                     a = types.identifier(id);
@@ -177,6 +255,10 @@ class MeiTua {
                     d.isHandle = true;
                     e = types.ifStatement(b, consequent, types.blockStatement([d]));
                     e.isHandle = true;
+                    if (path.container === null) {
+                        path.parentPath.replaceWith(e);
+                        return;
+                    }
                     path.replaceWith(e);
                     return;
                 }
@@ -235,6 +317,7 @@ class MeiTua {
                 if (!types.isForStatement(consequent.body[0])) return;
                 if (alternate !== null) return;
                 let byd = path.get('consequent.body.0.body.body.2');
+                if (byd === undefined) return;
                 let oyd = consequent.body[0].body.body[0].declarations[0].id.name;
                 let uyd = [];
                 this.mtg(byd, oyd);
@@ -267,6 +350,33 @@ class MeiTua {
         })
     }
 
+    yve() {
+        traverse(this.ast, {
+            BinaryExpression: (path) => {
+                let {parentPath, node} = path;
+                if (!types.isAssignmentExpression(parentPath.node)) return;
+                let {left, operator, right} = node;
+                if (!types.isAssignmentExpression(left)) return;
+                if (!types.isIdentifier(right)) return;
+                let uga = types.expressionStatement(left);
+                parentPath.insertBefore(uga);
+                path.node.left = left.left;
+            }
+        });
+        traverse(this.ast, {
+            UnaryExpression: (path) => {
+                let {parentPath, node} = path;
+                if (!types.isAssignmentExpression(parentPath.node)) return;
+                let {operator, argument} = node;
+                if (operator !== '~') return;
+                if (!types.isAssignmentExpression(argument)) return;
+                let lhw = types.expressionStatement(argument);
+                parentPath.insertBefore(lhw);
+                path.node.argument = argument.left
+            }
+        });
+    }
+
     start() {
         this.kis();
         this.jub();
@@ -274,6 +384,7 @@ class MeiTua {
         this.sky();
         this.igr();
         this.kuf();
+        this.yve();
         this.save_file();
     }
 
@@ -286,4 +397,5 @@ mt_ast.start();
 
 
 console.timeEnd('处理完毕，耗时');
+
 
